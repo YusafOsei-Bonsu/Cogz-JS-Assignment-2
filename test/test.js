4daa615763e2da446d8bb9b3f75a7bc2401b55e2
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-14 19:28:25 +0100

    Comment for test suites

--- test/test.js
+++ test/test.js
@@ -2,9 +2,13 @@ const assert = require('chai').assert;
 const expect = require('chai').expect;
 
 describe('JS Assignment 2', () => {
+
+    // Test suite for the Regex exercises
     describe('Regex Exercises', () => {
 
     });
+
+    // Test suite for the Promise exercises
     describe('Promise Exercises', () => {
 
     });

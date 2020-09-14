d095af4167649ce3ca5b2599c7792456c5f10de0
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-14 19:30:56 +0100

    Sample test data

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
+// Sample test data
+const ex1 = 'The quick brown fox jumped over the lazy dog';
+const ex2 = 'A1B2C3D4E5F6G7H8I9J10';
+const ex3 = 'The salad costs $9.99';
+const ex4 = 'Contact customer support on 0800 300 500';
+const ex5 = 'You can contact me on Twitter @codebubb or james@juniordevelopercentral.com';
+
 describe('JS Assignment 2', () => {
 
     // Test suite for the Regex exercises

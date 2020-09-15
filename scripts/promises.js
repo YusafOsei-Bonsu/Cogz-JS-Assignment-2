6393fd9cb75c71f04943d13ecef12302b5799fc2
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-15 13:13:19 +0100

    Promise Q3 code

--- scripts/promises.js
+++ scripts/promises.js
@@ -42,3 +42,42 @@ export const sortWords = capitalisedArr => {
         }
     });
 }
+
+/* Using Promise.all() & Promise.allSettled() */
+
+// Determines if the number is even
+const isEven = num => {
+    return new Promise((resolve, reject) => {
+        if (num % 2 == 0) {
+            resolve(`${num} is an even number`);
+        } else {
+            reject(`${num} is an odd number`);
+        }
+    });
+}
+
+// Determines if the number is prime
+const isPrime = num => {
+    return new Promise((resolve, reject) => {
+        if (num === 2) resolve(`${num} is prime`);
+        
+        for (let n = 2; n <= num-1; n++) {
+            if (num % n == 0) {
+                reject(`${num} is not prime`);
+            }
+        }
+
+        resolve(`${num} is prime`);
+    })
+}
+
+// Function that wants to determine if the provided number is an even, odd, prime or non-prime (via Promise.allSettled())
+export const evenPrimesSettled = async (num) => {
+    let result = await Promise.allSettled([isEven(num), isPrime(num)]);
+    return result;
+}
+
+export const evenPrimesAll = async (num) => {
+    let result = await Promise.all([isEven(num), isPrime(num)]);
+    return result;
+}

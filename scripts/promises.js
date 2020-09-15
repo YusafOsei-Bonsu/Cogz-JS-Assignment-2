36692dca6891d4b4bb33721a44e237bba0093572
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-15 09:08:42 +0100

    JS function that capitalises all strings of an array

--- scripts/promises.js
+++ scripts/promises.js
@@ -16,3 +16,21 @@ export const testNum = num => {
         }
     });
 }
+
+// Capitalises each word in an array
+export const makeAllCaps = wordArray => {
+    return new Promise((resolve, reject) => {
+        const isAlphabet = /^[a-zA-Z]+$/;
+        
+        // Creating new array that contains capitalised version of 
+        let capitalised = wordArray.map(word => {
+            // If 'word' is a string with only alphabets
+            if (typeof(word) == 'string' && isAlphabet.test(word)) {
+                return word.toUpperCase();
+            } else {
+                reject('Array contains at least one non-alphabetical string');
+            }
+        });
+        resolve(capitalised);
+    });
+}
